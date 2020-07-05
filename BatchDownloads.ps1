0f6218b117e1f9fff19600b736aa2f8c9c4cc06a
Rockey Wen <rockey@secretvisons.com>
2020-07-05 14:17:14 +1000

    Create BatchDownloads.ps1

---
+++ BatchDownloads.ps1
@@ -0,0 +1,36 @@
+﻿[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$false)]$DownloadList,
+    [Parameter(Mandatory=$false)]$DownloadPath
+)
+
+# default values of the parameters
+$_downloadTargetList = ".\DownloadList.txt";
+$_downloadTargetPath = ".\Downloads\";
+
+# global variable settings
+$downloadTargetList = $DownloadList;
+$downloadTargetPath = $DownloadPath;
+
+# control of the values of the global vairables
+if ($DownloadList -eq $null) {$downloadTargetList = $_downloadTargetList;}
+if ($DownloadPath -eq $null) {$downloadTargetPath = $_downloadTargetPath;}
+
+# prepair the destination download path
+if (!(Test-Path $downloadTargetPath)) {New-Item -ItemType "directory" -Path $downloadTargetPath;}
+
+# load the task lists
+$downloadList = Get-Content $downloadTargetList;
+
+# main scripts
+try{
+    foreach ($downloadItem in $downloadList) {
+        $downloadFileName = Split-Path $downloadItem -Leaf;
+        $downloadFile = $downloadTargetPath+$downloadFileName;
+        Invoke-WebRequest -uri $downloadItem -Method Get -OutFile $downloadFile;
+    }
+}
+catch{
+    if(!(Test-Path ".\Logs")) {New-Item -ItemType "directory" -Path ".\Logs";}
+    Add-Content -Value $PSItem.ToString() -Path ".\Logs\batch_download_error.log";
+}
