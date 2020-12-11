26f2bfd73aa8fc304f2d60f4709472741ad85b71
Rockey Wen <rockey@secretvisons.com>
2020-12-11 19:16:10 +1100

    v0.2

--- LocalSystemScan/LocalSystemScan.ps1
+++ LocalSystemScan/LocalSystemScan.ps1
@@ -1,48 +1,134 @@
-﻿$getRunningServices = $true;
-$getRunningProcesses = $true;
-$getFileSystemInfo = $true;
-$getRegistryInfo = $true;
+﻿Param(
+    [Parameter(Mandatory=$false,ValueFromPipeline=$false)][Boolean] $getRService,
+    [Parameter(Mandatory=$false,ValueFromPipeline=$false)][Boolean] $getRProcess,
+    [Parameter(Mandatory=$false,ValueFromPipeline=$false)][Boolean] $getDirInfo,
+    [Parameter(Mandatory=$false,ValueFromPipeline=$false)][Boolean] $getRegInfo,
+    [Parameter(Mandatory=$false,ValueFromPipeline=$false)][String[]] $outFormat,
+    [Parameter(Mandatory=$false,ValueFromPipeline=$false)][String[]] $inputLoc,
+    [Parameter(Mandatory=$false,ValueFromPipeline=$false)][String[]] $outputLoc
+)
+
+$getRunningServices = $false;
+$getRunningProcesses = $false;
+$getDirectoryInfo = $false;
+$getRegistryInfo = $false;
+$outputFormat = "JSonTXT";
 
 $currentLocation = Get-Location;
+$inputLocation = $currentLocation;
+$outputLocation = $currentLocation;
+$computeName = $env:COMPUTERNAME;
+$timestamp = [int][double]::Parse((Get-Date -UFormat %s));
 
-$dirRootListFile = ""+$currentLocation+"\directory_list.txt";
-$regRootListFile = ""+$currentLocation+"\registry_list.txt";
-
-$reportRunningService = ""+$currentLocation+"\Running_Services.txt";
-$reportRunningProcess = ""+$currentLocation+"\Running_Processes.txt";
-$reportFireDirProperty = ""+$currentLocation+"\directory_property.txt";
-$reportFireDirACL = ""+$currentLocation+"\directory_acl.txt";
-$reportRegistryProperty = ""+$currentLocation+"\registry_property.txt";
-$reportRegistryACL = ""+$currentLocation+"\registry_acl.txt";
-
-if ((Test-Path $reportRunningService) -eq $true) {Remove-Item -Path $reportRunningService -Force;}
-if ((Test-Path $reportRunningProcess) -eq $true) {Remove-Item -Path $reportRunningProcess -Force;}
-if ((Test-Path $reportFireDirProperty) -eq $true) {Remove-Item -Path $reportFireDirProperty -Force;}
-if ((Test-Path $reportFireDirACL) -eq $true) {Remove-Item -Path $reportFireDirACL -Force;}
-if ((Test-Path $reportRegistryProperty) -eq $true) {Remove-Item -Path $reportRegistryProperty -Force;}
-if ((Test-Path $reportRegistryACL) -eq $true) {Remove-Item -Path $reportRegistryACL -Force;}
-
-
-if ($getRunningServices -eq $true){
-    Get-Service * | ConvertTo-Json | Out-File $reportRunningService
+If ($getRService -ne $null) {
+    if (($getRService -eq $true) -or ($getRService -eq $false)) {$getRunningServices = $getRService;}
 }
 
-if ($getRunningProcesses -eq $true){
-    Get-Process * | ConvertTo-Json | Out-File $reportRunningProcess 
+If ($getRProcess -ne $null) {
+    if (($getRProcess -eq $true) -or ($getRProcess -eq $false)) {$getRunningProcesses = $getRProcess;}
 }
 
-if($getFileSystemInfo -eq $true){
-   $dirRootList = Get-Content $dirRootListFile
+If ($getDirInfo -ne $null) {
+    if (($getDirInfo -eq $true) -or ($getDirInfo -eq $false)) {$getDirectoryInfo = $getDirInfo;}
+}
+
+If ($getRegInfo -ne $null) {
+    if (($getRegInfo -eq $true) -or ($getRegInfo -eq $false)) {$getRegistryInfo = $getRegInfo;}
+}
+
+If ($outFormat -ne $null) {
+    if (($outFormat -eq "JsonTXT") -or ($outFormat -eq "Json") -or ($outFormat -eq "XML") -or ($outFormat -eq "TXT") -or ($outFormat -eq "HTML") -or ($outFormat -eq "CSV")) {$outputFormat = $outFormat;}
+}
+
+IF ($inputLoc -ne $null){
+    $dirInputFile = ""+$inputLoc+"\directory_list.txt";
+    $regInputFile = ""+$inputLoc+"\registry_list.txt";
+    if (((Test-Path $dirInputFile) -eq $true) -and ((Test-Path $regInputFile) -eq $true)) {
+        $inputLocation = $inputLoc;
+    }
+}
+
+IF ($outputLoc -ne $null){
+    if (((Test-Path $outputLoc) -eq $true)) {
+        $outputLocation = $outputLoc;
+    }
+}
+
+If (($getRunningServices -eq $false) -and ($getRunningProcesses -eq $false) -and ($getDirectoryInfo -eq $false) -and ($getRegistryInfo -eq $false)){
+    $getRunningServices = $true;
+    $getRunningProcesses = $true;
+    $getDirectoryInfo = $true;
+    $getRegistryInfo = $true;
+}
+
+$dirRootListFile = ""+$inputLocation+"\directory_list.txt";
+$regRootListFile = ""+$inputLocation+"\registry_list.txt";
+
+$outputFileFormat = ".txt";
+if ($outputFormat -eq "xml") {$outputFileFormat = ".xml";}
+if ($outputFormat -eq "html") {$outputFileFormat = ".html";}
+if ($outputFormat -eq "csv") {$outputFileFormat = ".csv";}
+if ($outputFormat -eq "json") {$outputFileFormat = ".json";}
+
+$reportRunningService = ""+$outputLocation+"\Running_Services_"+$computeName+"_"+$timestamp+$outputFileFormat+"";
+$reportRunningProcess = ""+$outputLocation+"\Running_Processes_"+$computeName+"_"+$timestamp+$outputFileFormat+"";
+$reportDirProperty = ""+$outputLocation+"\Directory_Properties_"+$computeName+"_"+$timestamp+$outputFileFormat+"";
+$reportDirPermission = ""+$outputLocation+"\Directory_Permissions_"+$computeName+"_"+$timestamp+$outputFileFormat+"";
+$reportRegProperty = ""+$outputLocation+"\Registry_Properties_"+$computeName+"_"+$timestamp+$outputFileFormat+"";
+$reportRegPermission = ""+$outputLocation+"\Registry_Permissions_"+$computeName+"_"+$timestamp+$outputFileFormat+"";
+
+function getRuningServices {
+    if (($outputFormat -eq "json") -or ($outputFormat -eq "jsontxt")) {Get-Service * | ConvertTo-Json | Out-File $reportRunningService}
+    if ($outputFormat -eq "csv") {Get-Service * | Select-Object -Property * | ConvertTo-Csv | Out-File $reportRunningService}
+    if ($outputFormat -eq "xml") {Get-Service * | Select-Object -Property * | ConvertTo-Xml | Export-Clixml $reportRunningService}
+    if ($outputFormat -eq "html") {Get-Service * | Select-Object -Property * | ConvertTo-Html | Out-File $reportRunningService}
+    if ($outputFormat -eq "txt") {Get-Service * | Select-Object -Property * | Format-Table -AutoSize -Wrap | Out-File $reportRunningService}
+}
+
+function getRunningProcesses {
+    if (($outputFormat -eq "json") -or ($outputFormat -eq "jsontxt")) {Get-Process * | ConvertTo-Json | Out-File $reportRunningProcess}
+    if ($outputFormat -eq "csv") {Get-Process * | Select-Object -Property * | ConvertTo-Csv | Out-File $reportRunningProcess}
+    if ($outputFormat -eq "xml") {Get-Process * | Select-Object -Property * | ConvertTo-Xml | Export-Clixml $reportRunningProcess}
+    if ($outputFormat -eq "html") {Get-Process * | Select-Object -Property * | ConvertTo-Html | Out-File $reportRunningProcess}
+    if ($outputFormat -eq "txt") {Get-Process * | Select-Object -Property * | Format-Table -AutoSize -Wrap | Out-File $reportRunningProcess}
+}
+
+function getDirectoryInformation {
+   $dirRootList = Get-Content $dirRootListFile;
    foreach ($dirList in $dirRootList){
-       $actionItem = Get-ChildItem -Path $dirList -Recurse
-       $actionItem | ForEach-Object -Begin $null -Process {Get-ItemProperty $_.PsPath | Select-Object -Property * -ExcludeProperty PSDrive,PSProvider,AccessRightType,AccessRuleType,AuditRightType,AuditRuleType,Sddl | ConvertTo-Json | Out-File -append $reportFireDirProperty },{Get-Acl $_.PsPath | Select-Object -Property * -ExcludeProperty PSDrive,PSProvider,AccessRightType,AccessRuleType,AuditRightType,AuditRuleType,Sddl | ConvertTo-Json | Out-File -append $reportFireDirACL } -End $null
+       $actionItem = Get-ChildItem -Path $dirList -Recurse;
+       if (($outputFormat -eq "json") -or ($outputFormat -eq "jsontxt")) {$actionItem | ForEach-Object -Begin $null -Process {Get-ItemProperty $_.PsPath | Select-Object -Property * -ExcludeProperty PSDrive,PSProvider,AccessRightType,AccessRuleType,AuditRightType,AuditRuleType,Sddl | ConvertTo-Json | Out-File -append $reportDirProperty },{Get-Acl $_.PsPath | Select-Object -Property * -ExcludeProperty PSDrive,PSProvider,AccessRightType,AccessRuleType,AuditRightType,AuditRuleType,Sddl | ConvertTo-Json | Out-File -append $reportDirPermission } -End $null}
+       if ($outputFormat -eq "csv") {$actionItem | ForEach-Object -Begin $null -Process {Get-ItemProperty $_.PsPath | Select-Object -Property * -ExcludeProperty PSDrive,PSProvider,AccessRightType,AccessRuleType,AuditRightType,AuditRuleType,Sddl | ConvertTo-Csv | Out-File -append $reportDirProperty },{Get-Acl $_.PsPath | Select-Object -Property * -ExcludeProperty PSDrive,PSProvider,AccessRightType,AccessRuleType,AuditRightType,AuditRuleType,Sddl | ConvertTo-Csv | Out-File -append $reportDirPermission } -End $null}
+       if ($outputFormat -eq "xml") {$actionItem | ForEach-Object -Begin $null -Process {Get-ItemProperty $_.PsPath | Select-Object -Property * -ExcludeProperty PSDrive,PSProvider,AccessRightType,AccessRuleType,AuditRightType,AuditRuleType,Sddl | ConvertTo-Xml | Export-Clixml -append $reportDirProperty },{Get-Acl $_.PsPath | Select-Object -Property * -ExcludeProperty PSDrive,PSProvider,AccessRightType,AccessRuleType,AuditRightType,AuditRuleType,Sddl | ConvertTo-Xml | Export-Clixml -append $reportDirPermission } -End $null}
+       if ($outputFormat -eq "html") {$actionItem | ForEach-Object -Begin $null -Process {Get-ItemProperty $_.PsPath | Select-Object -Property * -ExcludeProperty PSDrive,PSProvider,AccessRightType,AccessRuleType,AuditRightType,AuditRuleType,Sddl | ConvertTo-Html | Out-File -append $reportDirProperty },{Get-Acl $_.PsPath | Select-Object -Property * -ExcludeProperty PSDrive,PSProvider,AccessRightType,AccessRuleType,AuditRightType,AuditRuleType,Sddl | ConvertTo-Html | Out-File -append $reportDirPermission } -End $null}
+       if ($outputFormat -eq "txt") {$actionItem | ForEach-Object -Begin $null -Process {Get-ItemProperty $_.PsPath | Select-Object -Property * -ExcludeProperty PSDrive,PSProvider,AccessRightType,AccessRuleType,AuditRightType,AuditRuleType,Sddl | Format-Table -AutoSize -Wrap | Out-File -append $reportDirProperty },{Get-Acl $_.PsPath | Select-Object -Property * -ExcludeProperty PSDrive,PSProvider,AccessRightType,AccessRuleType,AuditRightType,AuditRuleType,Sddl | Format-Table -AutoSize -Wrap | Out-File -append $reportDirPermission } -End $null}
    }
 }
 
-if($getRegistryInfo -eq $true){
+function getRegistryInformation {
    $regRootList = Get-Content $regRootListFile
    foreach ($regList in $regRootList){
        $actionItem = Get-ChildItem -Path Registry::$regList -Recurse
-       $actionItem | ForEach-Object -Begin $null -Process {Get-ItemProperty $_.PsPath | Select-Object -Property * -ExcludeProperty PSDrive,PSProvider | ConvertTo-Json | Out-File -append $reportRegistryProperty },{Get-Acl $_.PsPath | Select-Object -Property * -ExcludeProperty PSDrive,PSProvider,AccessRightType,AccessRuleType,AuditRightType,AuditRuleType,Sddl | ConvertTo-Json | Out-File -append $reportRegistryACL } -End $null
+       if (($outputFormat -eq "json") -or ($outputFormat -eq "jsontxt")) {$actionItem | ForEach-Object -Begin $null -Process {Get-ItemProperty $_.PsPath | Select-Object -Property * -ExcludeProperty PSDrive,PSProvider | ConvertTo-Json | Out-File -append $reportRegProperty },{Get-Acl $_.PsPath | Select-Object -Property * -ExcludeProperty PSDrive,PSProvider,Access,AccessRightType,AccessRuleType,AuditRightType,AuditRuleType,Sddl | ConvertTo-Json | Out-File -append $reportRegPermission } -End $null}
+       if ($outputFormat -eq "csv") {$actionItem | ForEach-Object -Begin $null -Process {Get-ItemProperty $_.PsPath | Select-Object -Property * -ExcludeProperty PSDrive,PSProvider | ConvertTo-Csv | Out-File -append $reportRegProperty },{Get-Acl $_.PsPath | Select-Object -Property * -ExcludeProperty PSDrive,PSProvider,Access,AccessRightType,AccessRuleType,AuditRightType,AuditRuleType,Sddl | ConvertTo-Csv | Out-File -append $reportRegPermission } -End $null}
+       if ($outputFormat -eq "xml") {$actionItem | ForEach-Object -Begin $null -Process {Get-ItemProperty $_.PsPath | Select-Object -Property * -ExcludeProperty PSDrive,PSProvider | ConvertTo-Xml | Export-Clixml -append $reportRegProperty },{Get-Acl $_.PsPath | Select-Object -Property * -ExcludeProperty PSDrive,PSProvider,Access,AccessRightType,AccessRuleType,AuditRightType,AuditRuleType,Sddl | ConvertTo-Xml | Export-Clixml -append $reportRegPermission } -End $null}
+       if ($outputFormat -eq "html") {$actionItem | ForEach-Object -Begin $null -Process {Get-ItemProperty $_.PsPath | Select-Object -Property * -ExcludeProperty PSDrive,PSProvider | ConvertTo-Html | Out-File -append $reportRegProperty },{Get-Acl $_.PsPath | Select-Object -Property * -ExcludeProperty PSDrive,PSProvider,Access,AccessRightType,AccessRuleType,AuditRightType,AuditRuleType,Sddl | ConvertTo-Html | Out-File -append $reportRegPermission } -End $null}
+       if ($outputFormat -eq "txt") {$actionItem | ForEach-Object -Begin $null -Process {Get-ItemProperty $_.PsPath | Select-Object -Property * -ExcludeProperty PSDrive,PSProvider | Format-Table -AutoSize -Wrap | Out-File -append $reportRegProperty },{Get-Acl $_.PsPath | Select-Object -Property * -ExcludeProperty PSDrive,PSProvider,Access,AccessRightType,AccessRuleType,AuditRightType,AuditRuleType,Sddl | Format-Table -AutoSize -Wrap | Out-File -append $reportRegPermission } -End $null}
    }
+}
+
+if ($getRunningServices -eq $true){
+    getRuningServices;
+}
+
+if ($getRunningProcesses -eq $true){
+    getRunningProcesses;
+}
+
+if($getDirectoryInfo -eq $true){
+    getDirectoryInformation;
+}
+
+if($getRegistryInfo -eq $true){
+    getRegistryInformation;
 }
